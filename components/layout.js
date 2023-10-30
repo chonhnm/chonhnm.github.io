1b594867a35bfbb94400ec35ae56801973b17ed8
chon <chonhnm@gmail.com>
2023-10-30 22:29:53 +0800

    cssstyle

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
@@ -24,40 +23,17 @@ export default function Layout({ children, home }) {
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="stylesheet" href="github-markdown.css"></link> */}
       </Head>
       <header className={styles.header}>
-        {home ? (
+        {home && (
           <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt=""
-            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt=""
-              />
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
-                {name}
-              </Link>
-            </h2>
           </>
         )}
       </header>
+      {!home && (<Link href="/">← Back to home</Link>)}
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/global.css'
+import '../styles/githubmarkdown.css'
 
 export default function App({Component, pageProps}) {
     return <Component {...pageProps} />;
